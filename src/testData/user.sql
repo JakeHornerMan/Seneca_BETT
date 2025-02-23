ff7c607d5326c8a3d2d75fd25702bb7694f876a6
Jake Horner <jakehorner19@gmail.com>
2025-02-23 16:26:48 +0000

    test data users

---
+++ src/testData/user.sql
@@ -0,0 +1,49 @@
+INSERT INTO "user"("username", "email", "password", "role", "createdAt") 
+VALUES 
+(
+    'alice', 
+    'alice.smith@example.com', 
+    '$2b$10$qx3TOx6stb8q/AXszLGkh.6gh7TCtEI1/Hp8NHBtQw/Sjb7l1Sa3G', 
+    'user', 
+    DEFAULT
+);
+
+INSERT INTO "user"("username", "email", "password", "role", "createdAt") 
+VALUES 
+(
+    'bob', 
+    'bob.johnson@example.com', 
+    '$2b$10$qx3TOx6stb8q/AXszLGkh.6gh7TCtEI1/Hp8NHBtQw/Sjb7l1Sa3G', 
+    'user', 
+    DEFAULT
+);
+
+INSERT INTO "user"("username", "email", "password", "role", "createdAt") 
+VALUES 
+(
+    'charlie', 
+    'charlie.brown@example.com', 
+    '$2b$10$qx3TOx6stb8q/AXszLGkh.6gh7TCtEI1/Hp8NHBtQw/Sjb7l1Sa3G', 
+    'user', 
+    DEFAULT
+);
+
+INSERT INTO "user"("username", "email", "password", "role", "createdAt") 
+VALUES 
+(
+    'david', 
+    'david.williams@example.com', 
+    '$2b$10$qx3TOx6stb8q/AXszLGkh.6gh7TCtEI1/Hp8NHBtQw/Sjb7l1Sa3G', 
+    'user', 
+    DEFAULT
+);
+
+INSERT INTO "user"("username", "email", "password", "role", "createdAt") 
+VALUES 
+(
+    'admin', 
+    'admin@example.com', 
+    '$2b$10$qx3TOx6stb8q/AXszLGkh.6gh7TCtEI1/Hp8NHBtQw/Sjb7l1Sa3G',
+    'admin', 
+    DEFAULT
+);
