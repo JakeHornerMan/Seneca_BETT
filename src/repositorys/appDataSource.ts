df32773b6d38d1e0d05f665e6b184df29ca410e5
Jake Horner <jakehorner19@gmail.com>
2025-02-24 00:14:18 +0000

    Update appDataSource.ts

--- src/repositorys/appDataSource.ts
+++ src/repositorys/appDataSource.ts
@@ -7,14 +7,18 @@ import { ModuleStats } from '../models/ModuleStats';
 
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 5432;
+const username = process.env.DB_USERNAME || "postgres";
+const password = process.env.DB_PASSWORD || "password";
+const database = process.env.DB_DATABASE || "postgres";
+const logging = process.env.DB_LOGGING || false;
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: host,
     port: Number(port),
-    username: "postgres",
-    password: "password",
-    database: "postgres",
+    username: username,
+    password: password,
+    database: database,
     synchronize: true,
     logging: false,
     entities: [User, Course, SessionStats, ModuleStats],
